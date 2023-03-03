a04fab1cd9a9c76078969586b33092fcc66ad39d
Carlosnoe10 <carlosdnoem@gmail.com>
2023-03-03 12:02:29 -0800

    YA casi he terminado las clases solo me falta empezar el login

---
+++ src/Artistas.java
@@ -0,0 +1,8 @@
+
+import java.util.ArrayList;
+
+public class Artistas extends Usuarios{
+public String nombreArtistico;
+public ArrayList<Canciones> CancionesSubidas;
+public ArrayList<Albums> AlbumsPublica2; 
+}
---
+++ src/Oyentes.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+
+public class Oyentes extends Usuarios{
+public ArrayList<Canciones> ListaDeMeGustas;
+public ArrayList<Canciones> ListaCreada;
+public ArrayList<Albunes> ListaDeAlbunesLike;
+
+}
